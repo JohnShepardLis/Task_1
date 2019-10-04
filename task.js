519d01c4943b703b47d2ec1124412312fb7b00cd
John <johnshepardlisa@gmail.com>
2019-10-04 11:53:47 +0800

    function task

--- task.js
+++ task.js
@@ -1,5 +1,15 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while(isNaN(money) || money == '' || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    }
+}
+start();
+
 
 let appData = {
     budget: money,
@@ -7,10 +17,10 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-
+function chooseExpenses() {
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", + ""),
         b = prompt("Во сколько обойдется?", '');
@@ -21,14 +31,20 @@ for (let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
      } else { 
          console.log("no");
-        i--; 
-     } 
-};
+            i--; 
+         } 
+    }
+}
+chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30);
+function detectDayBudget() {
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+}
+detectDayBudget();
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay );
 
+function detectLevel() {
 if(appData.moneyPerDay < 100) {
     console.log("Минимальный уровень достатка");
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
@@ -37,8 +53,28 @@ if(appData.moneyPerDay < 100) {
     console.log("Высокий уровень достатка");
 } else {
     console.log("Произошла ошибка");
-};
+    }
+}
+detectLevel();
 
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений?'),
+            percent = +prompt('Под какой процент?');
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+    for (let i = 0; i <3; i++) {
+        let opt = +prompt("Статья необязательных расходов?");
+    }
+}
+chooseOptExpenses();
 
 /*let i = 0;
 
