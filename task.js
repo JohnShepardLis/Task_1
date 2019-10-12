f2a3e5b87822ff77f85ca05c8db03c4cef0234fb
John <johnshepardlisa@gmail.com>
2019-10-12 12:24:19 +0800

    Task7

--- task.js
+++ task.js
@@ -1,3 +1,26 @@
+let calculation = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName("budget-value")[0],
+    dayBudgetValue = document.getElementsByClassName("daybudget-value")[0],
+    levelNewValue = document.getElementsByClassName("level-value")[0],
+    expensesValue = document.getElementsByClassName("expenses-value")[0],
+    optionalValue = document.getElementsByClassName("optionalexpenses-value")[0],
+    incomeValue = document.getElementsByClassName("income-value")[0],
+    monthSavingsValue = document.getElementsByClassName("monthsavings-value")[0],
+    yearSavingsValue = document.getElementsByClassName("yearsavings-value")[0],
+
+    input = document.getElementsByClassName("expenses-item"),
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    chooseIncomeLabel = document.querySelector(".choose-income-labe"),
+    checkSavings = document.querySelector('#savings'),
+    chooseSum = document.querySelector(".choose-sum"),
+    choosePercent = document.querySelector(".choose-percent"),
+    yearValue = document.querySelector(".year-value"),
+    monthValue = document.querySelector(".month-value"),
+    dayValue = document.querySelector(".day-value");
+    
 let money, time;
 
 function start() {
